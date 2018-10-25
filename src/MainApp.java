de18d8c043c9ba6082d5dd46a6b659070d164dd2
AjaBurnett <burnettaja@gmail.com>
2018-10-25 13:57:46 -0400

    this is how it SHOULD be done lol

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
 	public static void main(String[] args) {
 System.out.println("Hi I'm Aja. It's pronounced Asia :-)");		// TODO Auto-generated method stub
 System.out.println("Hello World");
-System.out.println("Hey Aja");
+System.out.println("Hey Aja. Who?...me?");
 
 	}
 
