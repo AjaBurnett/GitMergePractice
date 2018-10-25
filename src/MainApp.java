343faab6dac36c44df432db887d9dc629b97f835
AjaBurnett <burnettaja@gmail.com>
2018-10-25 13:44:52 -0400

    Added a line of code

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,9 +2,10 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+System.out.println("Hi I'm Aja. It's pronounced Asia :-)");		// TODO Auto-generated method stub
 System.out.println("Hello World");
 System.out.println("Hey Aja");
+
 	}
 
 }
