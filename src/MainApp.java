d08186b72654a48a4801aa2db4d8c588580f0714
AjaBurnett <burnettaja@gmail.com>
2018-10-25 14:04:05 -0400

    corrected

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,12 +4,9 @@ public class MainApp {
 	public static void main(String[] args) {
 System.out.println("Hi I'm Aja. It's pronounced Asia :-)" );		// TODO Auto-generated method stub
 System.out.println("Hello World");
-<<<<<<< HEAD
-System.out.println("Hey Aja. Who?...me?");
-=======
+
 System.out.println("Hey Aja" + " I'm Ben");
 
->>>>>>> e1e3c42d05dfb5ffa610b3738f2f10a7e1c6e35f
 
 	}
 
