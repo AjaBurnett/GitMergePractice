e1e3c42d05dfb5ffa610b3738f2f10a7e1c6e35f
bjscalise <bjscalise11@gmail.com>
2018-10-25 13:56:15 -0400

    added I'm Ben

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,9 +2,10 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-System.out.println("Hi I'm Aja. It's pronounced Asia :-)");		// TODO Auto-generated method stub
+System.out.println("Hi I'm Aja. It's pronounced Asia :-)" );		// TODO Auto-generated method stub
 System.out.println("Hello World");
-System.out.println("Hey Aja");
+System.out.println("Hey Aja" + " I'm Ben");
+
 
 	}
 
